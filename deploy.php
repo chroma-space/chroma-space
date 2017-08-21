913e5ce43f49ec5218067269c1a64632b0de9589
tchapi <regbasket+github@gmail.com>
2017-08-21 11:05:54 +0200

    Fixes

--- deploy.php
+++ deploy.php
@@ -14,6 +14,7 @@ set('http_user', 'www-data');
 set('default_stage', 'production');
 set('repository', 'git@github.com:tchapi/chroma-space.git');
 set('writable_dirs', ['cache', 'logs', 'images', 'user', 'backup', 'tmp', 'user/pages', 'user/data']);
+set('writable_mode', "chown");
 set('shared_dirs', ['user/data', 'user/pages', 'backup']);
 set('clear_paths', [
   './README.md',
