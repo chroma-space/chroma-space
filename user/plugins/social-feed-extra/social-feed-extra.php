a5176c7c880bf840f808671fb0b4fd382bb158f8
tchapi <regbasket+github@gmail.com>
2019-01-26 23:10:16 +0100

    Add Twig extension as separate plugin
    Fix access token

---
+++ user/plugins/social-feed-extra/social-feed-extra.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+use Grav\Plugin\SocialFeedExtra\Twig\PostExtraExtension;
+
+/**
+ * Class SocialFeedExtraPlugin.
+ */
+class SocialFeedExtraPlugin extends Plugin
+{
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0],
+        ];
+    }
+
+    /**
+     * Initialize the plugin.
+     */
+    public function onPluginsInitialized()
+    {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+
+        require_once __DIR__.'/vendor/autoload.php';
+
+        $this->enable([
+            'onTwigExtensions' => ['onTwigExtensions', 0],
+        ]);
+    }
+
+    /**
+     * Add Twig Extensions.
+     */
+    public function onTwigExtensions()
+    {
+        $this->grav['twig']->twig->addExtension(new PostExtraExtension());
+    }
+}
---
+++ user/plugins/social-feed-extra/src/Twig/PostExtraExtension.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Grav\Plugin\SocialFeedExtra\Twig;
+
+use Grav\Common\Page\Collection;
+use Grav\Plugin\SocialFeed\Manager\PostManager;
+
+/**
+ * PostExtraExtension allows to fetch posts from twig templates.
+ */
+class PostExtraExtension extends \Twig_Extension
+{
+    /**
+     * Returns extension name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'PostExtraExtension';
+    }
+
+    /**
+     * Adding filters.
+     *
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('aggregate', [$this, 'aggregateSocialPostsWithPages']),
+        ];
+    }
+
+    /**
+     * Get posts and mix them with pages
+     *
+     * @param Collection $pages
+     *
+     * @return array
+     */
+    public function aggregateSocialPostsWithPages(Collection $pages)
+    {
+        $manager = new PostManager();
+        $socialPosts = $manager->getPosts();
+
+        $aggregated = [];
+        foreach ($pages as $page) {
+            $aggregated[] = [
+                "type" => "page",
+                "date" => $page->date(),
+                "data" => $page,
+            ];
+        }
+
+        foreach ($socialPosts["posts"] as $post) {
+            $aggregated[] = [
+                "type" => "post",
+                "provider" => $post["provider"],
+                "date" => (new \DateTime($post["publishedAt"]))->getTimestamp(),
+                "data" => $post,
+            ];
+        }
+
+        uasort($aggregated, function($a,$b) { return $a["date"] < $b["date"]; });
+
+        return $aggregated;
+    }
+}
